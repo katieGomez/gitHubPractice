640126a2dd6de0f84d6a407d1791bdf15a6de950
Katie Gomez <kgomez0177@gmail.com>
2023-07-13 11:26:11 -0700

    Initial Commit

---
+++ gitHubPractice/gitHubPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitHubPracticeApp.swift
+//  gitHubPractice
+//
+//  Created by Scholar on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct gitHubPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
